2e3d9329e6ed1da952aeef705e938fc8b4a939f2
androCyber <tatlicak@gmail.com>
2022-10-26 16:19:25 +0300

    15.Defining our first routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    return "<h1>All Contacts</h1>";
+});
+
+Route::get('/contact/create', function () {
+    return "<h1>Add Contact</h1>";
+});
