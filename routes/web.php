b3aca59ebe4c220ec1555a499924055580e971c8
androCyber <tatlicak@gmail.com>
2022-10-29 21:51:57 +0300

    18. Named routes

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $html="
+    <h1>
+    Contact App
+    </h1>
+    <div>
+        <a  href=".route('contact.index').">All Contact </a>
+        <a  href=".route('contact.create').">Add Contact</a>
+        <a  href=".route('contact.show',1523).">Show Contact</a>
+    </div>
+    ";
+    return $html;
 });
 
 /* Route Examples */
 Route::get('/contacts', function () {
     return "<h1>All Contacts</h1>";
-});
+})->name('contact.index');
 
 Route::get('/contacts/create', function () {
     return "<h1>Add Contact</h1>";
-});
+})->name('contact.create');
 
 /* Route Parameters */
 
 Route::get('contacts/{id}', function ($id) {
     return "Contact ID: ".$id;
-})->whereNumber("id");
+})->whereNumber("id")->name('contact.show');
 //->where('id','[0-9]+'); //for Numeric 
 
 Route::get('companies/{name?}', function ($name=null) {
@@ -43,6 +53,6 @@ Route::get('companies/{name?}', function ($name=null) {
 
         return "All Companies";
     }
-})->whereAlphaNumeric("name");
+})->whereAlphaNumeric("name")->name('contact.name');
 //->whereAlpha("name");//for Alpha
 //->where('name','[a-zA-Z]+'); //for Alpha
