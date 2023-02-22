6343c87d7ab67a7d44b21f545e59324246b09128
androCyber <tatlicak@gmail.com>
2023-02-22 12:06:54 +0300

    Creating Pagination manually

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Repositories\CompanyRepository;
-use Illuminate\Http\Request;
 use App\Models\Contact;
+use Illuminate\Http\Request;
+use App\Repositories\CompanyRepository;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ContactController extends Controller
 {
@@ -28,7 +29,14 @@ class ContactController extends Controller
         ]; */
     
         $companies=$this->company->pluck();
-        $contacts=Contact::latest()->paginate(10);
+       // $contacts=Contact::latest()->paginate(10);
+        $contactsCollection=Contact::latest()->get();
+        $perPage=10;
+        $currentPage=request()->query('page',1);
+        $items = $contactsCollection->slice(($currentPage*$perPage)-$perPage,$perPage);
+        $total= $contactsCollection->count();
+
+       $contacts= new LengthAwarePaginator($items, $total, $perPage, $currentPage,['path'=> request()->url(),'query'=> request()->query()]);
 
     
         return view('contacts.index',compact('contacts','companies'));
