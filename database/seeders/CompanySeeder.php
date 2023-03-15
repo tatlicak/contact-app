43f6aeacff2cb49d8cb8c9c8a21b0fd14c2afddb
androCyber <tatlicak@gmail.com>
2023-03-15 06:53:33 +0300

    Factories completed

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Faker\Factory as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CompanySeeder extends Seeder
 {
@@ -16,23 +17,6 @@ class CompanySeeder extends Seeder
      */
     public function run()
     {
-        $companies=[];
-        $faker=Faker::create();
-
-        foreach(range(1,10) as $index){
-
-                $company=[
-
-                    'name' => $faker->company(),
-                  'address'=>$faker->address(),
-                  'website'=>$faker->domainName(),
-                  'email'=>$faker->email(), 
-                  'created_at'=>now(),
-                  'updated_at'=>now()
-                ];
-                $companies[]=$company;
-        }
-        DB::table('companies')->delete();
-        DB::table('companies')->insert($companies);
+       Company::factory()->count(10)->create();
     }
 }
--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -19,25 +19,6 @@ class ContactSeeder extends Seeder
   
         public function run()
         {
-          $companies = Company::all();
-          $faker = Faker::create();
-          $contacts = [];
-  
-          foreach ($companies as $company) {
-              foreach (range(1, 5) as $index) {
-                  $contact = [
-                      'first_name' => $faker->firstName(),
-                      'last_name' => $faker->lastName(),
-                      'phone' => $faker->phoneNumber(),
-                      'email' => $faker->email(),
-                      'address' => $faker->address(),
-                      'company_id' => $company->id,
-                      'created_at' => now(),
-                      'updated_at' => now(),
-                  ];
-                  $contacts[] = $contact;
-              }
-          }
-          Contact::insert($contacts);
+         Contact::factory()->count(100)->create();
         }
 }
